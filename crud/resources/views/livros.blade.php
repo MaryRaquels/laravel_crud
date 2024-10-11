1d000fe978af8af379fb9131a05808071613a236
MaryRaquels <maria.lima2794@aluno.ce.gov.br>
2024-10-11 11:55:30 -0300

    tá responsivo

--- crud/resources/views/livros.blade.php
+++ crud/resources/views/livros.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
 
-<div class="container mt-3">
-  <h2>Livros</h2>
+<div class="container mt-3 justify-content-center align-items-center">
   <table class="table table-dark table-striped">
     <thead>
       <tr>
@@ -16,16 +15,21 @@
       </tr>
     </thead>
     <tbody>
+      <div class="justify-content-center align-text-center">
         @foreach ($livros as $livro)
-        <tr>
-            <td>{{ $livro->id }}</td>
-            <td>{{ $livro->titulo }}</td>
-            <td>{{ $livro->autor }}</td>
-            <td>{{ $livro->paginas }}</td>
-            <td><button class="btn btn-danger">Deletar</button></td>
-            <td><button class="btn btn-success">Atualizar</button></td>
-        </tr>
+          <tr>
+              <td>{{ $livro->id }}</td>
+              <td>{{ $livro->titulo }}</td>
+              <td>{{ $livro->autor }}</td>
+              <td>{{ $livro->paginas }}</td>
+              <td><button class="btn btn-danger">Deletar</button></td>
+              <td><button class="btn btn-success">Atualizar</button></td>
+          </tr>
         @endforeach
+        <div class="justify-content-center align-items-end">
+        <button class="btn btn-primary align-items-end">Adicionar</button>
+        </div>
+        </div>
     </tbody>
   </table>
 </div>
